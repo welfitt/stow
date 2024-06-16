f5a87028c947c99258fea178b5ffadb059031fd6
Adrian W Nicholls <welfitt@gmail.com>
2024-06-16 18:07:54 +0100

    added bash and a test dir for lua

---
+++ lua/test.lua
@@ -0,0 +1,13 @@
+
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+
+if file_exists("testa.lua")
+then
+   print("file exists")
+else
+   print("file does not exist")
+end
