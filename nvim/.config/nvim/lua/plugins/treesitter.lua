b108dbb933b6602bbe612193ddc6ebafc25bf874
Adrian W Nicholls <welfitt@gmail.com>
2024-06-18 10:34:31 +0100

    added a bit for terraform

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -18,6 +18,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+          ensure_installed = { "terraform", "hcl" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
