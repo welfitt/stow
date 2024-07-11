10e24bf6e8dd7a640ddfc884153e80173ec58e18
Adrian W Nicholls <welfitt@gmail.com>
2024-07-11 17:08:46 +0100

    Changed shortcuts for git

--- nvim/.config/nvim/lua/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			require("gitsigns").setup()
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
 			vim.keymap.set("n", "<leader>gs", ":Gitsigns toggle_signs<CR>", {})
 			vim.keymap.set("n", "<leader>gh", ":Gitsigns toggle_linehl<CR>", {})
 			vim.keymap.set("n", "<leader>gl", ":Gitsigns setloclist<CR>", {})
